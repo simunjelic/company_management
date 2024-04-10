520a50edd8e1982d1dd1e047f3ba00baf7cc3571
Šimo <simun.jelic@gmail.com>
2024-04-10 07:51:27 +0200

    Login Logic done

---
+++ Praksa_projectV1/DataAccess/UserRepository.cs
@@ -0,0 +1,67 @@
+﻿using Praksa_projectV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praksa_projectV1.DataAccess
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly Context _dbContext;
+        public void add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+            bool validUser = false;
+
+            // Create a new instance of your DbContext
+            using (var dbContext = new Context())
+            {
+                // Check if there is a user with the provided username and password
+                var user = dbContext.Users.FirstOrDefault(u => u.Username == credential.UserName && u.Password == credential.Password);
+
+                // If a user is found, set validUser to true
+                if (user != null)
+                {
+                    validUser = true;     
+                }
+            }
+
+            return validUser;
+            
+        }
+
+        public IEnumerable<User> getAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User getByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User getUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void remove(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+       
+    }
---
+++ Praksa_projectV1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,106 @@
+﻿using Praksa_projectV1.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Praksa_projectV1.ViewModels
+{
+    public class LoginViewModel: ViewModelBase
+    {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+        private IUserRepository userRepository;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get
+            {
+                return _isViewVisible;
+            }
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+        //-> Commands
+        public ICommand LoginCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        //Constructor
+        public LoginViewModel()
+        {
+            userRepository=new UserRepository();
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 ||
+                Password == null || Password.Length < 3)
+                validData = false;
+            else
+                validData = true;
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            var isValidUser = userRepository.AuthenticateUser(new System.Net.NetworkCredential(Username, Password));
+            if(isValidUser)
+            {
+                Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(Username),null);
+                IsViewVisible = false;
+            }else
+            {
+                ErrorMessage = "*Invalid username or password";
+            }
+        }
+    }
+    
+}
