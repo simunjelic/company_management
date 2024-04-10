9c1eb75d0d7a93dac581fb84836405d564c502fd
Šimo <simun.jelic@gmail.com>
2024-04-10 16:03:49 +0200

    Main menu

---
+++ Praksa_projectV1/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Praksa_projectV1.Models;
+using Praksa_projectV1.DataAccess;
+using System.Threading;
+
+namespace Praksa_projectV1.ViewModels
+{
+    public class MainViewModel: ViewModelBase
+    {
+        //Fields
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get
+            {
+                return _currentUserAccount;
+            }
+
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            
+            
+                CurrentUserAccount.Username = Thread.CurrentPrincipal.Identity.Name;
+                CurrentUserAccount.DisplayName = $"Welcome {Thread.CurrentPrincipal.Identity.Name} ;)";
+                CurrentUserAccount.ProfilePicture = null;
+            
+        }
+
+    }
+}
